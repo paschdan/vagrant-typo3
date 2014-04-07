f7b6e94dc39d2e2537a68905988ac79024bbe035
paschdan <daniel.paschke@wirkaufens.de>
2014-04-08 00:10:34 +0200

    removed typo3

--- vagrant/puppet/modules/typo3/manifests/params.pp
+++
@@ -1,8 +0,0 @@
-class typo3::params {
-    #general settings
-    $version = '6.2.0'
-    $download_dir = '/opt'
-    $download_url =  'http://prdownloads.sourceforge.net/typo3'
-    $site_name = 'cms'
-    $site_type = 'typo3_src'
-}
